7775f5a3bd59614f32cd4f78cd101e6d294dbc74
LucasLutf <oolucaslutf@gmail.com>
2022-09-21 22:53:13 -0300

    novo

--- cep/src/views/CepView.vue
+++ cep/src/views/CepView.vue
@@ -20,24 +20,60 @@ export default {
   <header class="corpo">
     <h1>Buscar CEP</h1>
     <form @submit.prevent="buscar">
-      <input type="text" v-model="cep" placeholder="Digite o CEP" />
-      <button type="submit">Buscar</button>
+      <input
+        type="text"
+        class="pesq"
+        v-model="cep"
+        placeholder="Digite o CEP"
+      />
+      <button type="submit" class="btn">Buscar</button>
     </form>
     <form class="end">
       <label for="rua">Rua</label>
       <input type="text" v-model="endereco.logradouro" />
-      <label for="num">Número</label>
-      <input type="text" v-model="endereco.numero" />
+      <label for="num">Cidade</label>
+      <input type="text" v-model="endereco.localidade" />
       <label for="bairro">Bairro</label>
       <input type="text" v-model="endereco.bairro" />
     </form>
-    {{ endereco }}
   </header>
 </template>
 
 <style>
+.corpo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+}
 .end {
   display: flex;
   flex-direction: column;
+  width: 15%;
+}
+h1 {
+  font-size: 30px;
+  font-style: oblique;
+}
+.btn {
+  background-color: #18a818;
+  color: #fafafa;
+  border: #44b934;
+  border-radius: 5px;
+  height: 30px;
+  width: 80px;
+  font-weight: bold;
+  cursor: pointer;
+  margin: 0 0 0 10px;
+}
+.btn:hover {
+  background-color: #23cf23;
+}
+.pesq {
+  width: 60%;
+  height: 25px;
+  border: 2px solid #ccc;
+  border-radius: 5px;
 }
 </style>
